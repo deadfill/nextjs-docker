0bacd694510134acc5c18d18bba0b8014f5c1bf2
Dmitrii <12fill12@mail.ru>
2023-04-20 20:01:48 +0200

    fix connect typesense

--- pages/api/addProduct.ts
+++ pages/api/addProduct.ts
@@ -7,7 +7,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 const client = new Typesense.Client({
   nodes: [
     {
-      host: 'localhost',
+      host: 'typesense',
       port: 8108,
       protocol: 'http',
     },
@@ -18,35 +18,30 @@ const client = new Typesense.Client({
 
 export default async function addProduct(req: NextApiRequest, res: NextApiResponse) {
   try {
-    // console.log('CONNECTING TO MONGO');
-    // await connectMongo();
-    // console.log('CONNECTED TO MONGO');
-
-    // console.log('CREATING DOCUMENT');
-    // const product = await Product.create(req.body);
-    // console.log('CREATED DOCUMENT');
-
-    // console.log("создание индекса typesense")
-
-    // res.json({ product });
     console.log('CONNECTING TO MONGO');
+    await connectMongo();
     console.log('CONNECTED TO MONGO');
 
     console.log('CREATING DOCUMENT');
+    const product = await Product.create(req.body);
     console.log('CREATED DOCUMENT');
-    const product = req.body;
-    
 
-    console.log("создание индекса typesense");
-    product["category"] = product["category.lvl"];
-    delete product["category.lvl"];
-
-    client.collections('products').documents().upsert(product);
-    console.log("complete");
-
-    console.log(product);
+    console.log("создание индекса typesense")
 
     res.json({ product });
+    // const product = req.body;
+    
+
+    // console.log("создание индекса typesense");
+    // product["category"] = product["category.lvl"];
+    // delete product["category.lvl"];
+
+    // client.collections('products').documents().upsert(product);
+    // console.log("complete");
+
+    // console.log(product);
+
+    // res.json({ product });
   } catch (error) {
     console.log(error);
     res.json({ error });
